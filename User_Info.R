05a22530786b9a1eca2e7fd9e1b28ea9c410c2eb
sarachronister <32340866+sarachronister@users.noreply.github.com>
2018-09-11 14:20:36 -0700

    Added User_Info.R

---
+++ User_Info.R
@@ -0,0 +1,6 @@
+# Input your username and password in the appropriate rows in place of the ********** 
+# and save to your home folder on the RStudio Server
+
+password <- dput(charToRaw("********"))
+UserID <- dput(charToRaw("*********"))
+username <- dput(charToRaw("BIOSENSE\\*********"))
